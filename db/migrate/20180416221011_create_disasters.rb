59ef4924d886b9ed7fca28779c9a83bfdf519703
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-04-16 15:24:02 -0700

    add database fields to disaster model in accordance with CSV file

--- db/migrate/20180416221011_create_disasters.rb
+++ db/migrate/20180416221011_create_disasters.rb
@@ -1,7 +1,24 @@
 class CreateDisasters < ActiveRecord::Migration[5.1]
   def change
     create_table :disasters do |t|
-
+      t.integer :disaster_number
+      t.integer :ih_program_declared
+      t.integer :ia_program_declared
+      t.integer :pa_program_declared
+      t.integer :hm_program_declared
+      t.string :state
+      t.string :declaration_date
+      t.integer :fy_declared
+      t.string :disaster_type
+      t.string :incident_type
+      t.string :title
+      t.string :incident_begin_date
+      t.string :incident_end_date
+      t.string :disaster_close_out_date
+      t.string :disaster_county_area
+      t.string :place_code
+      t.string :hash
+      t.string :last_refresh
       t.timestamps
     end
   end
